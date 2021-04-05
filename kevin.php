7ff9ea2ceedb7a15c0e20fccc851d0ad1f7fca9d
kevin enrique <floreskevin881@gmail.com>
2021-04-05 00:10:58 -0500

    cambios realizados

--- kevin.php
+++ kevin.php
@@ -1,3 +1,5 @@
-Nombre: Flores Armenta Kevin Enrique <br>
-Telefono: 5580939092 <br>
-Correo: floreskevin881@gmail.com <br>
+<html>
+<h1>Nombre: Flores Armenta Kevin Enrique</h1><br>
+<h2>Telefono: 5580939092</h2> <br>
+<h3>Correo: floreskevin881@gmail.com</h3> <br>
+</html>
