dd6a919e7bea5a68de86fe06709aa2749ef9cc9e
Alfredo Castillejos <acastillejos@dixi-project.com>
2021-04-07 17:33:05 -0500

    Subir cambios .

---
+++ index.php
@@ -0,0 +1,12 @@
+Aqui realizare mi tarea
+Cambio eb la tarea 
+cambio nuevo
+Armando razo
+Nancy eliseth Tapia Aragon
+Jose Benito Reyes
+ELIZABETH OROPEZA
+hola soy luis 
+Alfredo Castillejos
+kevin enrique
+Hola soy Chino
+Una nueva linea para reverse
